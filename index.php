acb527f73ce5385abe85ef6a968a1e3990b7d02d
Anas Ansari <anasanzari@gmail.com>
2016-01-21 22:28:35 +0530

    initial

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>Autism</title>
+  <link href="./static/css/awe.css" rel="stylesheet">
+  <link href="./static/css/player.css" rel="stylesheet">
+  <script type="text/javascript" src="./static/js/jquery-1.11.3.min.js"></script>
+  <script type="text/javascript" src="./static/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="./static/js/video.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="slc">
+  <div class="slope main">
+    <div class="scontent">
+      <h1>Autism Community</h1>
+      <h2>Something about this organization</h2>
+    </div>
+  </div>
+  <div class="about">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 col-sm-12">
+          <h1>About Autism</h2>
+          <p>Autism spectrum disorder (ASD) is a complex developmental disability; signs typically appear during early childhood and affect a person’s ability to communicate,
+             and interact with others.ASD is defined by a certain set of behaviors and is a “spectrum condition” that affects individuals differently and to varying degrees.
+             </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 vid">
+          <video id="my-video" class="video-js raised" controls preload="auto" style="width:100%"
+            poster="./videos/thumbs/autism.jpg" data-setup="{}">
+              <source src="./videos/autism.mp4" type='video/mp4'>
+              <p class="vjs-no-js">
+                To view this video please enable JavaScript, and consider upgrading to a web browser that
+                <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+              </p>
+          </video>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="community">
+  <div class="container-fluid">
+          <div class="row quote">
+              <div class="col-md-8 col-md-offset-2">
+                <h1>Autism Community</h1>
+                <p><em>“I think that the best thing we can do for our children is to allow them to do things for themselves,
+                  allow them to be strong, allow them to experience life on their own terms, allow them to take the subway...
+                  let them be better people, let them believe more in themselves.”</em> </p>
+                  <p class="pull-right"> - C. JoyBell C.</p>
+                </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4">
+              <img class="img-responsive" src="./static/images/boy.jpg" />
+            </div>
+            <div class="col-md-8">
+              <h1>About Us</h2>
+              <p>The Autism Community, the nation’s leading grassroots autism organization, exists to improve the lives of all affected by autism.
+                 We do this by increasing public awareness about the day-to-day issues faced by people on the spectrum,
+                 advocating for appropriate services for individuals across the lifespan,
+                and providing the latest information regarding treatment, education, research and advocacy.</p>
+            </div>
+          </div>
+    </div>
+</div>
+
+<div class="services container-fluid">
+
+        <div class="row tiles">
+          <h1>Services</h1>
+          <div class="col-md-4">
+            <img src="./static/images/icons/talk.png" />
+            <h3>Online training</h3>
+          </div>
+          <div class="col-md-4">
+            <img  src="./static/images/icons/videos.png" />
+            <h3>Watch videos</h3>
+          </div>
+          <div class="col-md-4">
+            <img src="./static/images/icons/interact.png" />
+            <h3>Interact with child</h3>
+          </div>
+        </div>
+</div>
+
+<div class="contact">
+  <div class="slope tilt">
+      <div class="scontent">
+        <h1>Contact Us</h1>
+        <p>Autism Community<br>
+        Somewhere between the streets<br>
+        Bangalore, Karnataka<br>
+        India</p>
+        <p>
+          Email : auticom@acorg.in</br>
+          Phone : 8956127568
+        </p>
+      </div>
+  </div>
+</div>
+
+
+
+</body>
+</html>
