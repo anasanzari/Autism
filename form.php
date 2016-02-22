62d6f27f897c1f5fdc2794b546ab018032537123
srjobinar <jobinar007@gmail.com>
2016-02-22 23:03:39 +0530

    form

---
+++ form.php
@@ -0,0 +1,415 @@
+<?php
+$f=0;
+if(isset($_POST['submit']))
+{
+  $f=1;
+  $sum=0;
+  $i=1;
+  for($i;$i<6;$i++)
+  {
+    $radio = "r".$i;
+    $sum = $sum + $_POST[$radio];
+  }
+}
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Form</title>
+    <link href="./static/css/awe.css" rel="stylesheet">
+    <link href="./static/css/player.css" rel="stylesheet">
+
+
+
+ <style>
+   b{
+     font-size: 18px;
+   }
+ </style>
+
+  </head>
+  <body>
+    <div class="container">
+    <center>
+      <form method="post" action="form.php">
+      <table class="table table-striped">
+        <tr>
+          <th>Items</th>
+          <td>Rarely</td>
+          <td>Sometimes</td>
+          <td>Frequently</td>
+          <td>Mostly</td>
+          <td>Always</td>
+        </tr>
+        <tr>
+          <td colspan="6"><b>Social Relationship And Reciprocity</b></td>
+        </tr>
+        <tr>
+          <td>1. Has Poor Eye Contact</td>
+          <th><input type="radio" name="r1" value="1"></th>
+          <th><input type="radio" name="r1" value="2"></th>
+          <th><input type="radio" name="r1" value="3"></th>
+          <th><input type="radio" name="r1" value="4"></th>
+          <th><input type="radio" name="r1" value="5"></th>
+        </tr>
+        <tr>
+          <td>2. Lacks Social Smile</td>
+          <th><input type="radio" name="r2" value="1"></th>
+          <th><input type="radio" name="r2" value="2"></th>
+          <th><input type="radio" name="r2" value="3"></th>
+          <th><input type="radio" name="r2" value="4"></th>
+          <th><input type="radio" name="r2" value="5"></th>
+        </tr>
+        <tr>
+          <td>3. Remains Aloof</td>
+          <th><input type="radio" name="r3" value="1"></th>
+          <th><input type="radio" name="r3" value="2"></th>
+          <th><input type="radio" name="r3" value="3"></th>
+          <th><input type="radio" name="r3" value="4"></th>
+          <th><input type="radio" name="r3" value="5"></th>
+        </tr>
+        <tr>
+          <td>4. Does Not Reach Out To Others</td>
+          <th><input type="radio" name="r4" value="1"></th>
+          <th><input type="radio" name="r4" value="2"></th>
+          <th><input type="radio" name="r4" value="3"></th>
+          <th><input type="radio" name="r4" value="4"></th>
+          <th><input type="radio" name="r4" value="5"></th>
+        </tr>
+        <tr>
+          <td>5. Unable To Relate To People</td>
+          <th><input type="radio" name="r5" value="1"></th>
+          <th><input type="radio" name="r5" value="2"></th>
+          <th><input type="radio" name="r5" value="3"></th>
+          <th><input type="radio" name="r5" value="4"></th>
+          <th><input type="radio" name="r5" value="5"></th>
+        </tr>
+        <tr>
+          <td>6. Unable To Respond To Socio/Environmental Cues</td>
+          <th><input type="radio" name="r6" value="1"></th>
+          <th><input type="radio" name="r6" value="2"></th>
+          <th><input type="radio" name="r6" value="3"></th>
+          <th><input type="radio" name="r6" value="4"></th>
+          <th><input type="radio" name="r6" value="5"></th>
+        </tr>
+        <tr>
+          <td>7. Engages In Solitary And Repetative Play Activities</td>
+          <th><input type="radio" name="r7" value="1"></th>
+          <th><input type="radio" name="r7" value="2"></th>
+          <th><input type="radio" name="r7" value="3"></th>
+          <th><input type="radio" name="r7" value="4"></th>
+          <th><input type="radio" name="r7" value="5"></th>
+        </tr>
+        <tr>
+          <td>8. Unable To Take Turns In Social Interactions</td>
+          <th><input type="radio" name="r8" value="1"></th>
+          <th><input type="radio" name="r8" value="2"></th>
+          <th><input type="radio" name="r8" value="3"></th>
+          <th><input type="radio" name="r8" value="4"></th>
+          <th><input type="radio" name="r8" value="5"></th>
+        </tr>
+        <tr>
+          <td>9. Does Not Maintain Peer Relationship</td>
+          <th><input type="radio" name="r9" value="1"></th>
+          <th><input type="radio" name="r9" value="2"></th>
+          <th><input type="radio" name="r9" value="3"></th>
+          <th><input type="radio" name="r9" value="4"></th>
+          <th><input type="radio" name="r9" value="5"></th>
+        </tr>
+        <tr>
+          <td colspan="6"><b>Emotional Responsiveness</b></td>
+        </tr>
+        <tr>
+          <td>10. Shows Inappropriate Imotional Response</td>
+          <th><input type="radio" name="r10" value="1"></th>
+          <th><input type="radio" name="r10" value="2"></th>
+          <th><input type="radio" name="r10" value="3"></th>
+          <th><input type="radio" name="r10" value="4"></th>
+          <th><input type="radio" name="r10" value="5"></th>
+        </tr>
+        <tr>
+          <td>11. Shows Exaggerated Emotions</td>
+          <th><input type="radio" name="r11" value="1"></th>
+          <th><input type="radio" name="r11" value="2"></th>
+          <th><input type="radio" name="r11" value="3"></th>
+          <th><input type="radio" name="r11" value="4"></th>
+          <th><input type="radio" name="r11" value="5"></th>
+        </tr>
+        <tr>
+          <td>12. Engages In Self Stimulating Emotions</td>
+          <th><input type="radio" name="r12" value="1"></th>
+          <th><input type="radio" name="r12" value="2"></th>
+          <th><input type="radio" name="r12" value="3"></th>
+          <th><input type="radio" name="r12" value="4"></th>
+          <th><input type="radio" name="r12" value="5"></th>
+        </tr>
+        <tr>
+          <td>13. Lacks Fear Of Danger</td>
+          <th><input type="radio" name="r13" value="1"></th>
+          <th><input type="radio" name="r13" value="2"></th>
+          <th><input type="radio" name="r13" value="3"></th>
+          <th><input type="radio" name="r13" value="4"></th>
+          <th><input type="radio" name="r13" value="5"></th>
+        </tr>
+        <tr>
+          <td>14. Exited Or Agitated For No Apparent Reason</td>
+          <th><input type="radio" name="r14" value="1"></th>
+          <th><input type="radio" name="r14" value="2"></th>
+          <th><input type="radio" name="r14" value="3"></th>
+          <th><input type="radio" name="r14" value="4"></th>
+          <th><input type="radio" name="r14" value="5"></th>
+        </tr>
+        <tr>
+          <td colspan="6"><b>Speech-Language And Communication</b></td>
+        </tr>
+        <tr>
+          <td>15. Acquired Speech And Lost It</td>
+          <th><input type="radio" name="r15" value="1"></th>
+          <th><input type="radio" name="r15" value="2"></th>
+          <th><input type="radio" name="r15" value="3"></th>
+          <th><input type="radio" name="r15" value="4"></th>
+          <th><input type="radio" name="r15" value="5"></th>
+        </tr>
+        <tr>
+          <td>16. Has Difficulty In Using Non-Verbal Language Or Gestures To Communicate</td>
+          <th><input type="radio" name="r16" value="1"></th>
+          <th><input type="radio" name="r16" value="2"></th>
+          <th><input type="radio" name="r16" value="3"></th>
+          <th><input type="radio" name="r16" value="4"></th>
+          <th><input type="radio" name="r16" value="5"></th>
+        </tr>
+        <tr>
+          <td>17. Engages In Steriotyped Or Repitative Use Of Language</td>
+          <th><input type="radio" name="r17" value="1"></th>
+          <th><input type="radio" name="r17" value="2"></th>
+          <th><input type="radio" name="r17" value="3"></th>
+          <th><input type="radio" name="r17" value="4"></th>
+          <th><input type="radio" name="r17" value="5"></th>
+        </tr>
+        <tr>
+          <td>18. Engages In Echolalic Speech</td>
+          <th><input type="radio" name="r18" value="1"></th>
+          <th><input type="radio" name="r18" value="2"></th>
+          <th><input type="radio" name="r18" value="3"></th>
+          <th><input type="radio" name="r18" value="4"></th>
+          <th><input type="radio" name="r18" value="5"></th>
+        </tr>
+        <tr>
+          <td>19. Produces Infantile Squeals Or Unusual Noises </td>
+          <th><input type="radio" name="r19" value="1"></th>
+          <th><input type="radio" name="r19" value="2"></th>
+          <th><input type="radio" name="r19" value="3"></th>
+          <th><input type="radio" name="r19" value="4"></th>
+          <th><input type="radio" name="r19" value="5"></th>
+        </tr>
+        <tr>
+          <td>20. Unable To Initiate Or Sustain Conversation With Others</td>
+          <th><input type="radio" name="r20" value="1"></th>
+          <th><input type="radio" name="r20" value="2"></th>
+          <th><input type="radio" name="r20" value="3"></th>
+          <th><input type="radio" name="r20" value="4"></th>
+          <th><input type="radio" name="r20" value="5"></th>
+        </tr>
+        <tr>
+          <td>21. Uses Jargon Or Meaningless Words</td>
+          <th><input type="radio" name="r21" value="1"></th>
+          <th><input type="radio" name="r21" value="2"></th>
+          <th><input type="radio" name="r21" value="3"></th>
+          <th><input type="radio" name="r21" value="4"></th>
+          <th><input type="radio" name="r21" value="5"></th>
+        </tr>
+        <tr>
+          <td>22. Uses Pronoun Reversals</td>
+          <th><input type="radio" name="r22" value="1"></th>
+          <th><input type="radio" name="r22" value="2"></th>
+          <th><input type="radio" name="r22" value="3"></th>
+          <th><input type="radio" name="r22" value="4"></th>
+          <th><input type="radio" name="r22" value="5"></th>
+        </tr>
+        <tr>
+          <td>23. Unable To Grasp Pragmatics Of Communication</td>
+          <th><input type="radio" name="r23" value="1"></th>
+          <th><input type="radio" name="r23" value="2"></th>
+          <th><input type="radio" name="r23" value="3"></th>
+          <th><input type="radio" name="r23" value="4"></th>
+          <th><input type="radio" name="r23" value="5"></th>
+        </tr>
+      <tr>
+        <td colspan="6"><b>Behaviour Patterns</b></td>
+      </tr>
+      <tr>
+        <td>24. Engages In Steriotyped And Repetative Motor Mannerism</td>
+        <th><input type="radio" name="r24" value="1"></th>
+        <th><input type="radio" name="r24" value="2"></th>
+        <th><input type="radio" name="r24" value="3"></th>
+        <th><input type="radio" name="r24" value="4"></th>
+        <th><input type="radio" name="r24" value="5"></th>
+      </tr>
+      <tr>
+        <td>25. Shows Attachment To Inanimate Objects</td>
+        <th><input type="radio" name="r25" value="1"></th>
+        <th><input type="radio" name="r25" value="2"></th>
+        <th><input type="radio" name="r25" value="3"></th>
+        <th><input type="radio" name="r25" value="4"></th>
+        <th><input type="radio" name="r25" value="5"></th>
+      </tr>
+      <tr>
+        <td>26. Shows Hyper Activity Or Restlessness</td>
+        <th><input type="radio" name="r26" value="1"></th>
+        <th><input type="radio" name="r26" value="2"></th>
+        <th><input type="radio" name="r26" value="3"></th>
+        <th><input type="radio" name="r26" value="4"></th>
+        <th><input type="radio" name="r26" value="5"></th>
+      </tr>
+      <tr>
+        <td>27. Exhibis Aggressive Behaviour</td>
+        <th><input type="radio" name="r27" value="1"></th>
+        <th><input type="radio" name="r27" value="2"></th>
+        <th><input type="radio" name="r27" value="3"></th>
+        <th><input type="radio" name="r27" value="4"></th>
+        <th><input type="radio" name="r27" value="5"></th>
+      </tr>
+      <tr>
+        <td>28. Throws Temper Tantrums</td>
+        <th><input type="radio" name="r28" value="1"></th>
+        <th><input type="radio" name="r28" value="2"></th>
+        <th><input type="radio" name="r28" value="3"></th>
+        <th><input type="radio" name="r28" value="4"></th>
+        <th><input type="radio" name="r28" value="5"></th>
+      </tr>
+      <tr>
+        <td>29. Engages In Self Injurious Behaviour</td>
+        <th><input type="radio" name="r29" value="1"></th>
+        <th><input type="radio" name="r29" value="2"></th>
+        <th><input type="radio" name="r29" value="3"></th>
+        <th><input type="radio" name="r29" value="4"></th>
+        <th><input type="radio" name="r29" value="5"></th>
+      </tr>
+      <tr>
+        <td>30. Insists On Sameness</td>
+        <th><input type="radio" name="r30" value="1"></th>
+        <th><input type="radio" name="r30" value="2"></th>
+        <th><input type="radio" name="r30" value="3"></th>
+        <th><input type="radio" name="r30" value="4"></th>
+        <th><input type="radio" name="r30" value="5"></th>
+      </tr>
+      <tr>
+        <td colspan="6"><b>Sensory Aspects</b></td>
+      </tr>
+      <tr>
+        <td>31. Unusually Sensitive To Sensory Stimuli</td>
+        <th><input type="radio" name="r31" value="1"></th>
+        <th><input type="radio" name="r31" value="2"></th>
+        <th><input type="radio" name="r31" value="3"></th>
+        <th><input type="radio" name="r31" value="4"></th>
+        <th><input type="radio" name="r31" value="5"></th>
+      </tr>
+      <tr>
+        <td>32. Stares Into Space For Long Period Of Time</td>
+        <th><input type="radio" name="r32" value="1"></th>
+        <th><input type="radio" name="r32" value="2"></th>
+        <th><input type="radio" name="r32" value="3"></th>
+        <th><input type="radio" name="r32" value="4"></th>
+        <th><input type="radio" name="r32" value="5"></th>
+      </tr>
+      <tr>
+        <td>33. Has Difficulty In Tracking Objects</td>
+        <th><input type="radio" name="r33" value="1"></th>
+        <th><input type="radio" name="r33" value="2"></th>
+        <th><input type="radio" name="r33" value="3"></th>
+        <th><input type="radio" name="r33" value="4"></th>
+        <th><input type="radio" name="r33" value="5"></th>
+      </tr>
+    <tr>
+      <td>34. Has Unusual Vision</td>
+      <th><input type="radio" name="r34" value="1"></th>
+      <th><input type="radio" name="r34" value="2"></th>
+      <th><input type="radio" name="r34" value="3"></th>
+      <th><input type="radio" name="r34" value="4"></th>
+      <th><input type="radio" name="r34" value="5"></th>
+    </tr>
+    <tr>
+      <td>35. Insensitive To Pain</td>
+      <th><input type="radio" name="r35" value="1"></th>
+      <th><input type="radio" name="r35" value="2"></th>
+      <th><input type="radio" name="r35" value="3"></th>
+      <th><input type="radio" name="r35" value="4"></th>
+      <th><input type="radio" name="r35" value="5"></th>
+    </tr>
+    <tr>
+      <td>36. Responds To Objects/People Unusually By Smelling,Touching Or Tasting</td>
+      <th><input type="radio" name="r36" value="1"></th>
+      <th><input type="radio" name="r36" value="2"></th>
+      <th><input type="radio" name="r36" value="3"></th>
+      <th><input type="radio" name="r36" value="4"></th>
+      <th><input type="radio" name="r36" value="5"></th>
+    </tr>
+    <tr>
+      <td colspan="6"><b>Cognitive Component</b></td>
+    </tr>
+    <tr>
+      <td>37. Inconsistent Attention And Concentration</td>
+      <th><input type="radio" name="r37" value="1"></th>
+      <th><input type="radio" name="r37" value="2"></th>
+      <th><input type="radio" name="r37" value="3"></th>
+      <th><input type="radio" name="r37" value="4"></th>
+      <th><input type="radio" name="r37" value="5"></th>
+    </tr>
+    <tr>
+      <td>38. Shows Delay In Responding</td>
+      <th><input type="radio" name="r38" value="1"></th>
+      <th><input type="radio" name="r38" value="2"></th>
+      <th><input type="radio" name="r38" value="3"></th>
+      <th><input type="radio" name="r38" value="4"></th>
+      <th><input type="radio" name="r38" value="5"></th>
+    </tr>
+    <tr>
+      <td>39. Has Unusual Memory Of Somekind</td>
+      <th><input type="radio" name="r39" value="1"></th>
+      <th><input type="radio" name="r39" value="2"></th>
+      <th><input type="radio" name="r39" value="3"></th>
+      <th><input type="radio" name="r39" value="4"></th>
+      <th><input type="radio" name="r39" value="5"></th>
+    </tr>
+    <tr>
+      <td>40. Has Savant Ability</td>
+      <th><input type="radio" name="r40" value="1"></th>
+      <th><input type="radio" name="r40" value="2"></th>
+      <th><input type="radio" name="r40" value="3"></th>
+      <th><input type="radio" name="r40" value="4"></th>
+      <th><input type="radio" name="r40" value="5"></th>
+    </tr>
+      </table>
+      <input type="submit" class="btn btn-success" name="submit" value="submit">
+    </form>
+    </center>
+    </div>
+
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Score</h4>
+      </div>
+      <div class="modal-body">
+        <?= $sum ?>
+      </div>
+    </div>
+  </div>
+</div>
+<script type="text/javascript" src="./static/js/jquery.min.js"></script>
+<script type="text/javascript" src="./static/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+      <?php
+      if($f==1)
+      {?>
+        $('.modal').modal('show');
+     <?php
+}    ?>
+    });
+</script>
+  </body>
+</html>
